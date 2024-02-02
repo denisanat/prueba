4749a866d65d4bb4bdd07cf3b1400189cef680b2
Denis Anatoliev <denisanatoliev03@gmail.com>
2024-02-02 01:45:03 +0100

    primer commit

---
+++ PruebaSync.java
@@ -0,0 +1,28 @@
+public class PruebaSync extends Thread {
+
+    public static int numero = 0;
+    private static Object lock = new Object();
+
+    public PruebaSync(String nombre) {
+        super(nombre);
+    }
+
+    public void run() {
+
+        synchronized ( lock ) {
+
+            numero++;
+            for (int i = 0; i < 10; i++)
+                System.out.println( getName() );
+
+            System.out.println( numero );
+
+        }
+    }
+
+    public static void main(String[] args) {
+        new PruebaSync("primer hilo").start();
+        new PruebaSync("segundo hilo").start();
+    }
+}
+
