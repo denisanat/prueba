d07cf3ebbd653c66050dd983fd94dcd0a222ffb1
Denis Anatoliev <denisanatoliev03@gmail.com>
2024-02-02 12:58:26 +0100

    Mas pruebas

--- carpeta1/HolaMundo.java
+++ carpeta1/HolaMundo.java
@@ -2,9 +2,9 @@ package carpeta1;
 
 public class HolaMundo {
 
-    static private int numero = 3;
 
     public static void main(String[] args) {
         System.out.println("Hola Mundo");
+
     }
 }
